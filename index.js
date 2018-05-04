7166ae09ea4528223a636eea864e8b1923ce70c0
Otso Jousimaa <otso@ojousima.net>
2018-05-04 19:38:38 +0300

    Add debug prin

--- index.js
+++ index.js
@@ -82,6 +82,7 @@ app.use((req, res, next) => {
     if(measurements.tags && Array.isArray(measurements.tags)){
      measurements.tags.forEach(function(sample){
       let data = ruuviParser.parse(sample.rawDataBlob);
+      console.log(byteToHexString(sample.rawDataBlob))
       data.rssi = sample.rssi;
       data.timestamp = sample.time;
 
