90f4a76287ff11259b34c330a608a12c3dbfd089
Otso Jousimaa <otso@ojousima.net>
2018-05-07 16:23:17 +0300

    Add fields

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
 const ruuviParser = require('ruuvi.endpoints.js');
 //GW sends malformed JSON, work around it
-const gwjsonParser = bodyParser.text({ type: 'application/json' });
+const gwjsonParser = bodyParser.text({
+  type: 'application/json'
+});
 const dJSON = require('dirty-json');
 
 // dJSON is async, patch express support
@@ -18,7 +20,7 @@ const config = require('./streamr-configuration.js')
 
 const data_port = config.data_port;
 const STREAM_ID = config.STREAM_ID;
-const API_KEY   = config.API_KEY;
+const API_KEY = config.API_KEY;
 
 // Create the streamr client and give the API key to use by default
 const StreamrClient = require('streamr-client')
@@ -27,18 +29,18 @@ const streamr_client = new StreamrClient({
 });
 
 // https://gist.github.com/tauzen/3d18825ae41ff3fc8981
-const byteToHexString = function (uint8arr) {
+const byteToHexString = function(uint8arr) {
   if (!uint8arr) {
     return '';
   }
-  
+
   var hexStr = '';
   for (var i = 0; i < uint8arr.length; i++) {
     var hex = (uint8arr[i] & 0xff).toString(16);
     hex = (hex.length === 1) ? '0' + hex : hex;
     hexStr += hex;
   }
-  
+
   return hexStr.toUpperCase();
 }
 
@@ -46,12 +48,12 @@ const hexStringToByte = function(str) {
   if (!str) {
     return new Uint8Array();
   }
-  
+
   var a = [];
-  for (var i = 0, len = str.length; i < len; i+=2) {
-    a.push(parseInt(str.substr(i,2),16));
+  for (var i = 0, len = str.length; i < len; i += 2) {
+    a.push(parseInt(str.substr(i, 2), 16));
   }
-  
+
   return new Uint8Array(a);
 }
 
@@ -87,112 +89,135 @@ app.use((req, res, next) => {
 /**
  * Handle data from RuuviStation app. 
  */
- app.post('/ruuvistation', jsonParser, function (req, res) {
+app.post('/ruuvistation', jsonParser, function(req, res) {
   let measurements = req.body;
 
-    // IF ruuvi station data
-    if(measurements.tags && Array.isArray(measurements.tags)){
-     measurements.tags.forEach(function(sample){
+  // IF ruuvi station data
+  if (measurements.tags && Array.isArray(measurements.tags)) {
+    measurements.tags.forEach(function(sample) {
       console.log(byteToHexString(sample.rawDataBlob.blob));
       let binary = sample.rawDataBlob.blob.slice(7);
+      console.log(byteToHexString(sample.rawDataBlob.blob));
       // Skip non-broadcast types
-      if(binary[0] < 2 || binary[0] > 5) { return; }
+      if (binary[0] < 2 || binary[0] > 5) {
+        return;
+      }
       let data = ruuviParser.parse(binary);
       data.rssi = sample.rssi;
-      data.timestamp = sample.time;
+      data.timestamp = new Date(sample.updateAt);
       data.mac = sample.id;
-      data.gateway = measurements.deviceId;
+      data.gateway_id = measurements.deviceId;
       console.log(data);
-      
+
       // Produce the event to the Stream
       streamr_client.produceToStream(config.STREAM_ID, data)
-        .then(() => function(){})
+        .then(() => function() {})
         .catch((err) => console.error(err))
-        });
-   }else {console.log("not an array"); }
-   res.send("ok");;
- });
+    });
+  } else {
+    console.log("not an array");
+  }
+  res.send("ok");;
+});
 
-app.post('/gateway', gwjsonParser, async function (req, res) {
+//         { timestamp: '2018-05-07T12:14:32Z',
+// type: 'Unknown',
+// mac: 'CA8AF83DDC7C',
+// bleName: '',
+// rssi: -51,
+// rawData: '02010415FF9904034A180BC907FFE8FFCC04200B8300000000' }
+
+app.post('/gateway', gwjsonParser, async function(req, res) {
   let str = req.body;
-  if(!str) 
-  { 
+  if (!str) {
     res.send("invalid");
     return;
   }
   let measurements = await dJSON.parse(str);
 
-    // IF GW data
-    if(Array.isArray(measurements)){
-      let influx_samples = [];
-      measurements.forEach(function(sample){
-        // print debug data to console TODO log file
-        if(sample.name === "gateway"){
-          console.log(sample.action);
-          //For each is a function call, "continue"
-          return;
-        }
+  // IF GW data
+  if (Array.isArray(measurements)) {
+    let influx_samples = [];
+    measurements.forEach(function(sample) {
+      // print debug data to console TODO log file
+      if (sample.name === "gateway") {
+        console.log(sample.action);
+        //For each is a function call, "continue"
+        return;
+      }
 
-        // Handle data points from RuuviTag broadcast formats
-        if(sample.type &&
-         sample.type === "Unknown" &&
-         sample.rawData &&
-         sample.rawData.includes("FF99040"))
-        {
-          console.log(sample);
-          let binary = hexStringToByte(sample.rawData.indexOf("FF99040") + 6);
-          let data = ruuviParser.parse(binary);
-        }
-      });
-    }else console.log("not an array");
+      // Handle data points from RuuviTag broadcast formats
+      if (sample.type &&
+        sample.type === "Unknown" &&
+        sample.rawData &&
+        sample.rawData.includes("FF99040")) {
+        console.log(sample);
+        let binary = hexStringToByte(sample.rawData.indexOf("FF99040") + 6);
+        let data = ruuviParser.parse(binary);
+        data.timestamp = new Date(sample.timestamp);
+        data.rssi = sample.rssi;
+        // format D6A911ADA763 into D6:A9:11:AD:A7:63
+        data.mac = sample.mac.match(/.{2}/g).join(":");
+        console.log(data);
 
-    res.send("ok");
-  });
+        // Produce the event to the Stream
+        streamr_client.produceToStream(config.STREAM_ID, data)
+          .then(() => function() {})
+          .catch((err) => console.error(err))
+
+      }
+    });
+  } else console.log("not an array");
+
+  res.send("ok");
+});
 
 
-app.post('/scanner', jsonParser, async function (req, res) {
+app.post('/scanner', jsonParser, async function(req, res) {
   let str = req.body;
-  if(!str) 
-  { 
+  if (!str) {
     res.send("invalid");
     return;
   }
   let measurements = req.body;
 
-  // IF GW data
-  if(Array.isArray(measurements)){
+  // IF scaaner data
+  if (Array.isArray(measurements)) {
     let streamr_samples = [];
-    measurements.forEach(function(sample){
+    measurements.forEach(function(sample) {
 
-       //Handle data points from broadcast formats
-       if(sample.type &&
-         sample.type === "Ruuvi"
-         && sample.dataPayload
-         && sample.dataPayload.startsWith("9904")) {
+      //Handle data points from broadcast formats
+      if (sample.type &&
+        sample.type === "Ruuvi" &&
+        sample.dataPayload &&
+        sample.dataPayload.startsWith("9904")) {
 
-         let data = ruuviParser.parse(Buffer.from(sample.dataPayload.substring(4), 'hex'));
-       data.timestamp = sample.timestamp;
-       data.rssi = sample.rssi;
-       console.log(data);
-         // Produce the event to the Stream
-         streamr_client.produceToStream(config.STREAM_ID, data)
-         .then(() => function(){})
-         .catch((err) => console.error(err))
-       }
-     });
-  } else { console.log("Incoming data is not an array"); }
+        let data = ruuviParser.parse(Buffer.from(sample.dataPayload.substring(4), 'hex'));
+        data.timestamp = sample.timestamp;
+        data.rssi = sample.rssi;
+        data.gateway_id = sample.gateway_id
+        console.log(data);
+        // Produce the event to the Stream
+        streamr_client.produceToStream(config.STREAM_ID, data)
+          .then(() => function() {})
+          .catch((err) => console.error(err))
+      }
+    });
+  } else {
+    console.log("Incoming data is not an array");
+  }
   res.send("ok");
 });
 
-http.createServer(app).listen(data_port, function () {
+http.createServer(app).listen(data_port, function() {
   console.log('Listening on port ' + data_port);
 });
 
 process
-.on('unhandledRejection', (reason, p) => {
-  console.error(reason, 'Unhandled Rejection at Promise', p);
-})
-.on('uncaughtException', err => {
-  console.error(err, 'Uncaught Exception thrown');
+  .on('unhandledRejection', (reason, p) => {
+    console.error(reason, 'Unhandled Rejection at Promise', p);
+  })
+  .on('uncaughtException', err => {
+    console.error(err, 'Uncaught Exception thrown');
     // process.exit(1);
   });
